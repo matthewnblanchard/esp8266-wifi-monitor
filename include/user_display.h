431874cd5681775c8c450e48692f54431b2ac1ba
Matthew Blanchard <mnblanchard49@gmail.com>
2017-12-06 14:04:23 -0500

    LCD now effectively displays letters/digits at set positions (8-pixel line aligned), and horizontal lines are drawable)

--- include/user_display.h
+++ include/user_display.h
@@ -21,12 +21,48 @@ void ICACHE_FLASH_ATTR user_lcd_init(os_event_t *e);
 //	Nothing
 void ICACHE_FLASH_ATTR user_lcd_cmd(uint8 cmd);
 
-// Application Function: user_lcd_data(uint8 data)
-// Desc: Sends one byte of data to the lcd
+// Application Function: user_lcd_update(void)
+// Desc: Updates the LCD with the display matrix
 // Args:
-//	uint8 data: Desired 8-bit data
+//	Nothing
 // Returns:
 //	Nothing 
-void ICACHE_FLASH_ATTR user_lcd_display(void);
+void ICACHE_FLASH_ATTR user_lcd_update(void);
+
+// Application Function: user_lcd_clear(void)
+// Desc: Clears the display matrix
+// Args:
+//	Nothing
+// Returns:
+//	Nothing
+void ICACHE_FLASH_ATTR user_lcd_clear(void);
+
+// Application Function: user_lcd_text(uint8 *text, uint16 len, uint16 line, uint16 offset);
+// Desc: Writes the given text to the input line at the input offset
+// Args:
+//	uint8 *text: Text string to be printed
+//	uint16 len: Length of text string to print
+//	uint16 line: Line on which to print
+//	uint16 offset: Pixel offset to print the text
+void ICACHE_FLASH_ATTR user_lcd_text(uint8 *text, uint16 len, uint16 line, uint16 offset);
+
+// Application Function: user_lcd_sniffer_xaxis(void);
+// Desc: Prints the x axis of the sniffer graph to the LCD
+// Args:
+//	None
+// Returns:
+//	Nothing
+void ICACHE_FLASH_ATTR user_lcd_sniffer_xaxis(void);
+
+// Application Function: user_lcd_hline(uint16 height, uint16 start_pos, uint16 end_pos);
+// Desc: Prints a horizontal line at the input height
+// Args:
+//	uint16 height: The desired height, starting from the TOP
+//		(e.g. height 0 is the top row of pixels)
+//	uint16 start_pos: Starting horizontal position
+//	uint16 end_pos: Ending horizontal position 
+// Returns:
+//	Nothing
+void ICACHE_FLASH_ATTR user_lcd_hline(uint16 height, uint16 start_pos, uint16 end_pos);
 
 #endif
--- user/src/user_display.c
+++ user/src/user_display.c
@@ -3,9 +3,52 @@
 
 uint8 display_matrix[1024];
 
+// 6 column text encoding. 
+char text_encoding[26][6] = {
+{0xF8, 0x14, 0x12, 0x14, 0xF8, 0x00},	// A
+{0xFE, 0x92, 0x92, 0x92, 0x6C, 0x00},	// B
+{0x38, 0x44, 0x82, 0x82, 0x44, 0x00},	// C
+{0xFE, 0x82, 0x82, 0x44, 0x38, 0x00}, 	// D
+{0xFE, 0x92, 0x92, 0x92, 0x92, 0x00},	// E
+{0xFE, 0x12, 0x12, 0x02, 0x02, 0x00},	// F
+{0x38, 0x44, 0x92, 0x92, 0x72, 0x00}, 	// G
+{0xFE, 0x10, 0x10, 0x10, 0xFE, 0x00},	// H
+{0x82, 0x82, 0xFE, 0x82, 0x82, 0x00},	// I
+{0x42, 0x82, 0x7E, 0x02, 0x02, 0x00},	// J
+{0xFE, 0x10, 0x28, 0x44, 0x82, 0x00},	// K
+{0xFE, 0x80, 0x80, 0x80, 0x80, 0x00},	// L
+{0xFC, 0x02, 0xFC, 0x02, 0xFC, 0x00},	// M
+{0xFE, 0x0C, 0x10, 0x60, 0xFE, 0x00},	// N
+{0x38, 0x44, 0x82, 0x44, 0x38, 0x00},	// O
+{0xFE, 0x12, 0x12, 0x12, 0x0C, 0x00},	// P
+{0x38, 0x44, 0xA2, 0x44, 0xB8, 0x00}, 	// Q
+{0xFE, 0x12, 0x32, 0x52, 0x8C, 0x00},	// R
+{0x4C, 0x92, 0x92, 0x92, 0x64, 0x00},	// S
+{0x02, 0x02, 0xFE, 0x02, 0x02, 0x00},	// T
+{0x7E, 0x80, 0x80, 0x80, 0x7E, 0x00}, 	// U
+{0x3E, 0x40, 0x80, 0x40, 0x3E, 0x00},	// V
+{0x7E, 0x80, 0x78, 0x80, 0x7E, 0x00},	// W
+{0xC6, 0x28, 0x10, 0x28, 0xC6, 0x00},	// X
+{0x06, 0x08, 0xF0, 0x08, 0x06, 0x00},	// Y
+{0x86, 0x8A, 0x92, 0xA2, 0xC2, 0x00}	// Z
+};
+
+// 4 column digit encoding
+char digit_encoding[10][4] = {
+{0xFE, 0x82, 0xFE, 0x00},	// 0
+{0x84, 0xFE, 0x80, 0x00},	// 1
+{0xF2, 0x92, 0x9E, 0x00},	// 2
+{0x92, 0x92, 0xFE, 0x00},	// 3
+{0x1E, 0x10, 0xFE, 0x00},	// 4
+{0x9E, 0x92, 0xF2, 0x00},	// 5
+{0xFE, 0x92, 0xF2, 0x00},	// 6
+{0x02, 0x02, 0xFE, 0x00},	// 7
+{0xFE, 0x92, 0xFE, 0x00},	// 8
+{0x1E, 0x12, 0xFE, 0x00}	// 9
+};
+
 void ICACHE_FLASH_ATTR user_lcd_init(os_event_t *e)
 {
-	os_memset(display_matrix, 0xFF, 1024);	// Initialize all pixels to 'on'
 
 	// Disable interrupts, this section is timing critical
 	ETS_GPIO_INTR_DISABLE();
@@ -35,8 +78,13 @@ void ICACHE_FLASH_ATTR user_lcd_init(os_event_t *e)
 	user_lcd_cmd(0x20);
 	user_lcd_cmd(0x00);
 
-	// Start by displaying all pixels	
-	user_lcd_display();
+	// Clear the LCD
+	user_lcd_clear();
+
+	user_lcd_sniffer_xaxis();
+
+	// Update the LCD
+	user_lcd_update();
 
 	// Re-enable interrupts
 	ETS_GPIO_INTR_ENABLE();
@@ -68,7 +116,7 @@ void ICACHE_FLASH_ATTR user_lcd_cmd(uint8 cmd)
 	return;	
 };
 
-void ICACHE_FLASH_ATTR user_lcd_display(void)
+void ICACHE_FLASH_ATTR user_lcd_update(void)
 {
 	uint16 i = 0;	// Loop index 
 	
@@ -108,4 +156,143 @@ void ICACHE_FLASH_ATTR user_lcd_display(void)
 	user_i2c_stop_bit();
 
 	return;	
+};
+
+void ICACHE_FLASH_ATTR user_lcd_clear(void)
+{
+	os_memset(display_matrix, 0x00, 1024);
+	return;
+};
+
+void ICACHE_FLASH_ATTR user_lcd_text(uint8 *text, uint16 len, uint16 line, uint16 offset)
+{	
+	uint16 pos = (128 * line) + offset;	// Starting column for printing
+	uint16 i = 0;				// Loop index
+	uint16 j = 0;				// Loop index 2
+	uint16 encode_index = 0;		// Index of a letter or digit in the encoding
+	bool is_digit = false;			// Indicates if a character is a digit (use digit encoding)
+
+	for (i = 0; i < len; i++) {
+
+		// Captical ASCII letter
+		if ((text[i] > 0x40) && (text[i] < 0x5B)) {
+			encode_index = text[i] - 0x41;
+			is_digit = false;
+		
+		// Lower case ASCII letter
+		} else if ((text[i] > 0x60) && (text[i] < 0x7B)) {
+			encode_index = text[i] - 0x61;
+			is_digit = false;
+
+		// ASCII digits
+		} else if ((text[i] > 0x2F) && (text[i] < 0x3A)) {
+			encode_index = text[i] - 0x30;
+			is_digit = true;
+
+		// Space - skip 4 columns, or to the next line if 4 columns exceeds the current line 
+		} else if (text[i] == 0x20) {
+			if (((pos % 128) + 6) > 127) {
+				 (pos + (128 - (pos % 128)));
+			} else {
+				pos += 4;
+			}
+			continue;	
+	
+		// Discard other characters
+		} else {
+			continue;
+		}
+	
+		// Letter printing	
+		if (!is_digit) {
+
+			// Skip to new line if there is not enough space
+			pos = (((pos % 128) + 6) > 127) ? (pos + (128 - (pos % 128))) : (pos);
+			
+			// Terminate if the next letter will run off the matrix
+			if ((pos + 6) >= (128 * 8)) break;
+
+			for (j = 0; j < 6; j++) {
+				display_matrix[pos] = text_encoding[encode_index][j];
+				pos++;
+			};
+		}
+
+		// Digit printing
+		if (is_digit) {
+
+			// Skip to new line if there is not enough space
+			pos = (((pos % 128) + 4) > 127) ? (pos + (128 - (pos % 128))) : (pos);
+
+			// Terminate if the next letter will run off the matrix
+			if ((pos + 4) >= (128 * 8)) break;
+
+			for (j = 0; j < 4; j++) {
+				display_matrix[pos] = digit_encoding[encode_index][j];
+				pos++;
+			};
+		}
+	}
+
+	return;
+};
+
+void ICACHE_FLASH_ATTR user_lcd_sniffer_xaxis(void)
+{
+	uint16 pos = 0;		// Display matrix position
+	uint8 channel = 1;	// Wifi channel #
+	uint8 digit = 0;	// Digit is display
+	
+	// Print x-axis label
+	user_lcd_text("wifi channel", 12, 7, 40);
+
+	// Print channel numbers at at spaced intervals
+	pos = 17;	// Starting position
+
+	// Single digit channels
+	for (channel = 1; channel <= 9; channel++) {
+		pos += 2;			// Space of padding
+		pos += 4;		// Skip a space
+		digit = channel + 0x30;	// Convert digit to ASCII
+		user_lcd_text(&digit, 1, 6, pos);	// Display digit
+		pos += 4;
+	};
+
+	// Double digit channels
+	for (channel = 10; channel <= 11; channel++) {
+		pos += 2;					// Space of padding
+		digit = 0x31;				// Display leading '1'
+		user_lcd_text(&digit, 1, 6, pos);	
+		pos += 4;				
+		digit = (channel % 10) + 0x30;		// Convert 1's place to ASCII
+		user_lcd_text(&digit, 1, 6, pos);
+		pos += 4;
+	};
+
+	// Draw hline at the bottom of the 6th row
+	user_lcd_hline(47, 17, 127);
+
+	return;
+};
+
+void ICACHE_FLASH_ATTR user_lcd_hline(uint16 height, uint16 start_pos, uint16 end_pos)
+{
+	uint16 i = 0;	// Loop index
+
+	// If the height is greater than the lowest row, discard
+	if (height > 63) {
+		return;
+	};
+
+	// Check that starting and ending positions
+	if ((start_pos > 127) || (end_pos > 127) || (end_pos <= start_pos)) {
+		return;
+	};  		
+
+	// Start at the beginning of a row
+	for (i = ((128 * (height / 8)) + start_pos); i < (((128 * (height / 8)) + 128) - (127 - end_pos)); i++) {
+		display_matrix[i] |= (0x01 << (height % 8));
+	};
+	
+	return;
 };
